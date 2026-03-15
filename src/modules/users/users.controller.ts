3407f761a30a4bd74b2cf4ba2d098103874e8ca3
Azizov1234 <baymirzayevadilnoza0@gmail.com>
2026-03-15 22:56:46 +0500

    add-users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller, Get, Post, Body, Patch, Param, Delete, 
+  UseGuards, UseInterceptors, UploadedFile, 
+  Req,
+  Put,
+  Query
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RolesGuard } from 'src/common/guards/checkRole.guard';
+import { Roles } from 'src/common/decorators/role.decorators';
+import { Role } from '@prisma/client';
+import { ApiBody, ApiConsumes, ApiQuery } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    schema: {
+      type: "object",
+      properties: {
+        firstName: { type: "string" },
+        lastName: { type: "string" },
+        email: { type: "string" },
+        username: { type: "string" },
+        password: { type: "string" },
+        avatar: { type: "string", format: "binary" }, // swagger uchun
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor("avatar"))
+  createAdminUser(
+    @Body() createUserDto: CreateUserDto,
+    @UploadedFile() file: Express.Multer.File, // file keladi
+  ) {
+    // Service ichida Cloudinary ga upload qiladi va URL saqlaydi
+    return this.usersService.createAdminUser(createUserDto, file);
   }
 
   @Get()
@@ -18,17 +51,37 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Req() req:Request) {
+    return this.usersService.findOneMe(req["user"].id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+   @Put()
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    schema: {
+      type: "object",
+      properties: {
+        firstName: { type: "string" },
+        lastName: { type: "string" },
+        username: { type: "string" },
+        avatar: { type: "string", format: "binary" },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor("avatar"))
+  updateProfile(
+    @Body() dto: UpdateUserDto,
+    @UploadedFile() file: Express.Multer.File,
+    @Req()req:Request
+  ) {
+    return this.usersService.updateProfile(req["user"].id, dto, file);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
-  }
+ 
+
+ 
+ 
+
+ 
+ 
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CloudinaryService } from 'src/modules/cloudinary/cloudinary.service';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/core/prisma/prisma.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    private prisma: PrismaService,
+    private cloudinaryService: CloudinaryService,
+  ) { }
+
+  async createAdminUser(
+    createUserDto: CreateUserDto,
+    file: Express.Multer.File,
+  ) {
+
+    const { email, username, password } = createUserDto;
+
+    const existingUser = await this.prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { username }],
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let avatarUrl: string | null = null;
+
+    if (file) {
+      const uploaded = await this.cloudinaryService.uploadImage(file);
+      avatarUrl = uploaded;
+    }
+
+    const user = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+        avatar: avatarUrl,
+        role: 'ADMIN',
+      },
+    });
+
+    return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneMe(id: number) {
+    const existMe = await this.prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+    if (!existMe) throw new NotFoundException("User is not found")
+    return {
+      success: true,
+      data: existMe
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async updateProfile(userId: number, dto: UpdateUserDto, file?: Express.Multer.File) {
+    let avatarUrl: string | undefined = undefined;
+
+    if (file) {
+
+      const uploaded = await this.cloudinaryService.uploadImage(file);
+      avatarUrl = uploaded
+    }
+
+    const updatedUser = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        ...dto,
+        ...(avatarUrl && { avatar: avatarUrl }),
+      },
+    });
+
+    return {
+      success: true,
+      message: "User succesfully updated"
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+
+
+    await this.prisma.user.delete({
+      where: { id: userId },
+    });
+
+    return { message: 'User removed successfully.' };
   }
 }
